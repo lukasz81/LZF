c9fd5a733132d78b66960d88d9b58b5919319b57
lukasz81 <info@designmasters.co.uk>
2018-06-20 14:12:38 +0100

    Add header structure / Add styling

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,8 +3,24 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-                <header className="header">
-                    <h1 className="title">Welcome to React</h1>
+                <header className='header display--flex'>
+                    <div className={'container display--flex'}>
+                        <h1 className="logo text--indent align-self--center">
+                            <a className={'display--block'} title={'Little Zebra Foundation'} href={'./'}>Little Zebra Foundation</a>
+                        </h1>
+                        <nav className={'align-self--center'}>
+                            <ul className={'display--flex text--transform-capitalize'}>
+                                <li className={'align-self--center'}><a href={''}>What we do</a></li>
+                                <li className={'align-self--center'}><a href={''}>Get Involved</a></li>
+                                <li className={'align-self--center'}><a href={''}>Shop</a></li>
+                                <li className={'align-self--center'}><a href={''}>News</a></li>
+                            </ul>
+                        </nav>
+                        <form className={'align-self--center'}>
+                            <input className={'search'} type={'search'} placeholder={'Search here'}/>
+                        </form>
+                        <button className={'cta text--transform-uppercase align-self--center'}>Donate now</button>
+                    </div>
                 </header>
         );
     }
