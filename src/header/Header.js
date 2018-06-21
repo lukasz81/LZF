d5c802818db7ae1a39fdc9f901f76555a87e3f69
lukasz81 <info@designmasters.co.uk>
2018-06-21 13:26:51 +0100

    Add styling / logic impovements to donataion widget

--- src/header/Header.js
+++ src/header/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
                             <a className={'display--block'} title={'Little Zebra Foundation'} href={'./'}>Little Zebra Foundation</a>
                         </h1>
                         <nav className={'align-self--center'}>
-                            <ul className={'display--flex text--transform-capitalize'}>
+                            <ul className={'display--flex text-transform--capitalize'}>
                                 <li className={'align-self--center'}><a href={''}>What we do</a></li>
                                 <li className={'align-self--center'}><a href={''}>Get Involved</a></li>
                                 <li className={'align-self--center'}><a href={''}>Shop</a></li>
@@ -20,7 +20,7 @@ class Header extends Component {
                         <form className={'align-self--center'}>
                             <input className={'search'} type={'search'} placeholder={'Search here'}/>
                         </form>
-                        <button className={'cta text--transform-uppercase align-self--center'}>Donate now</button>
+                        <button className={'cta text-transform--uppercase align-self--center'}>Donate now</button>
                     </div>
                 </header>
         );
--- src/widget/Widget.js
+++ src/widget/Widget.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {DONATION_VALUES} from './donationValues';
 import { connect } from 'react-redux';
 import {changeAnimalType} from '../actions';
+import './Widget.css';
 
 export class Widget extends Component {
 
@@ -10,6 +11,7 @@ export class Widget extends Component {
         this.state = {
             selectedDonation: DONATION_VALUES.defaultValue,
             lastSelectedDonation: DONATION_VALUES.defaultValue,
+            manualInputValue: '',
             isManualInputInError: false,
             hasSetMonthlyDonation: true
         };
@@ -20,6 +22,7 @@ export class Widget extends Component {
     };
 
     handleDonationChange = event => {
+        this.resetManualInputField(event);
         this.setState({
             selectedDonation: Number(event.target.value),
             lastSelectedDonation: Number(event.target.value)
@@ -36,6 +39,7 @@ export class Widget extends Component {
             this.resetRadioButtons(lastDonationRememberedValue)
         }
         this.setState({
+            manualInputValue: value,
             isManualInputInError: Number(value) < DONATION_VALUES.minManualValue && value !== ''
         });
 
@@ -53,6 +57,17 @@ export class Widget extends Component {
         });
     };
 
+    resetManualInputField = event => {
+        let value = Number(event.target.value);
+        if (value <  DONATION_VALUES.minManualValue) {
+            this.resetRadioButtons(this.state.lastSelectedDonation);
+            this.setState({
+                manualInputValue: '',
+                isManualInputInError: false
+            });
+        }
+    };
+
     //setState is async. Had to move the checking method to a function as the update to the state was step behind
     checkUpdatedValues = value => {
         return this.state.selectedDonation === value
@@ -66,53 +81,62 @@ export class Widget extends Component {
 
     render() {
         return (
-            <section className={'widget'}>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>
-                            <span>I want to help:</span>
-                            <select className={'select'} value={this.props.animal} onChange={this.handleSelectChange}>
-                                <option value="giraffe">a giraffe</option>
-                                <option value="rhino">a rhino</option>
-                                <option value="tiger">a tiger</option>
-                            </select>
-                        </label>
-                    </div>
-                    <div>
-                        <span>I want to donate:</span>
-                        <br/>
-                        {DONATION_VALUES.values.map((value,i) => {
-                            return (
-                                <label key={i}>
-                                    <input type="radio"
-                                           value={value}
-                                           onChange={this.handleDonationChange}
-                                           checked={this.checkUpdatedValues(value)}/>
-                                    £{value}
+            <React.Fragment>
+                <h2 className={'text-transform--uppercase text-align--center text-weight--bold donate-header'}>Donate today</h2>
+                <p className={'text-align--center donate-paragraph'}> Help protecting the planet today, become a Little Zebra ambassador</p>
+                <section className={'widget display--flex space-between'}>
+                    <div className={'form-container'}>
+                        <h3 className={'text-transform--uppercase text-align--center text-weight--bold form-header'}>Make a donation</h3>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className={'display--flex space-between'}>
+                                <span className={'align-self--center'}>I want to help:</span>
+                                <select className={'select donation-input'} value={this.props.animal} onChange={this.handleSelectChange}>
+                                    <option value="giraffe">a giraffe</option>
+                                    <option value="rhino">a rhino</option>
+                                    <option value="tiger">a tiger</option>
+                                </select>
+                            </div>
+                            <span className={'donation-span'}>I want to donate:</span>
+                            <div className={'display--flex space-between'}>
+                                {DONATION_VALUES.values.map((value,i) => {
+                                    return (
+                                        <label key={i} className={`radio-label display--inline-block text-align--center donation-input is-checked-${this.checkUpdatedValues(value)}`}>
+                                            <input type="radio"
+                                                   value={value}
+                                                   onChange={this.handleDonationChange}
+                                                   checked={this.checkUpdatedValues(value)}/>
+                                            <span>£{value}</span>
+                                        </label>
+                                    )
+                                })}
+                                <span className={'align-self--center'}>Or</span>
+                                <input className={this.state.isManualInputInError ? 'error donation-input' : 'donation-input'}
+                                       type="number"
+                                       min='11'
+                                       placeholder={'£'}
+                                       value={this.state.manualInputValue}
+                                       onBlur={this.resetManualInputField}
+                                       onChange={this.handleManualInput}/>
+                            </div>
+                            <div>
+                                <label>
+                                    <input type="checkbox"
+                                           onChange={this.handleCheckBoxChange}
+                                           checked={this.state.hasSetMonthlyDonation}/>
+                                    <span className={'donation-span'}>I want to do a monthly donation</span>
                                 </label>
-                            )
-                        })}
-                        <span> Or </span>
-                        <input className={this.state.isManualInputInError ? 'error' : ''}
-                               type="number"
-                               onChange={this.handleManualInput}/>
+                            </div>
+                            <input type="submit"
+                                   className={this.state.isManualInputInError ? 'cta error':'cta'}
+                                   disabled={this.state.isManualInputInError}
+                                   value="Donate now"/>
+                        </form>
                     </div>
-                    <div>
-                        <label>
-                            <input type="checkbox"
-                                   onChange={this.handleCheckBoxChange}
-                                   checked={this.state.hasSetMonthlyDonation}/>
-                            <span>I want to do a monthly donation</span>
-                        </label>
+                    <div className={`image-container ${this.props.animal} align-self--flex-end`}>
+                        <img alt={`${this.props.animal}`} src={`./images/${this.props.animal}-image.jpg`}/>
                     </div>
-                    <input type="submit"
-                           disabled={this.state.isManualInputInError}
-                           value="Submit"/>
-                </form>
-                <div className={`animal-container ${this.state.animal}`}>
-                    <img alt={`${this.state.animal}`} src={`./images/${this.props.animal}-image.jpg`}/>
-                </div>
-            </section>
+                </section>
+            </React.Fragment>
         );
     }
 }
