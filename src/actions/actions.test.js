eea4efa0d8c3b0cf4e5f1159f31227aea819c1c5
lukasz81 <info@designmasters.co.uk>
2018-06-26 16:22:13 +0100

    Add tests to actions

---
+++ src/actions/actions.test.js
@@ -0,0 +1,61 @@
+import configureMockStore from 'redux-mock-store';
+import * as actions from './';
+import * as types from './actionTypes';
+const mockStore = configureMockStore();
+
+describe('Redux actions', () => {
+
+    it('should create an action to set animal to "anaconda"', () => {
+
+        const expectedAction = [{
+            type: types.ANIMAL_TYPE,
+            animal: 'anaconda'
+        }];
+        const store = mockStore();
+
+        store.dispatch(actions.changeAnimalType('anaconda'));
+        expect(store.getActions()).toEqual(expectedAction);
+
+
+    });
+
+    it('should create an action to set donation', () => {
+
+        const expectedAction = [{
+            type: types.SET_DONATION_VALUE,
+            donation: {value:1,isSetManually:false}
+        }];
+        const store = mockStore();
+
+        store.dispatch(actions.setDonationValues({value:1,isSetManually:false}));
+        expect(store.getActions()).toEqual(expectedAction);
+
+    });
+
+    it('should create an action to set setToRegularDonations to "true"', () => {
+
+        const expectedAction = [{
+            type: types.SET_TO_REGULAR_DONATION,
+            isRegularDonation: true
+        }];
+        const store = mockStore();
+
+        store.dispatch(actions.setToRegularDonations(true));
+        expect(store.getActions()).toEqual(expectedAction);
+
+    });
+
+    it('should rememberLastDonationValue', () => {
+
+        const expectedAction = [{
+            type: types.REMEMBER_LAST_DONATION_VALUE,
+            value: 6
+        }];
+        const store = mockStore();
+
+        store.dispatch(actions.rememberLastDonationValue(6));
+        expect(store.getActions()).toEqual(expectedAction);
+
+    });
+
+});
